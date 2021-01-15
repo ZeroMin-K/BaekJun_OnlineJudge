ccfe148da697894790aa459e323150a5eaf0fe78
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-16 00:06:41 +0900

    Create 1546.py

---
+++ stage05/1546.py
@@ -0,0 +1,9 @@
+test = int(input())
+score_list = list(map(int, input().split()))
+
+max_score = max(score_list)
+
+for i in range(test):
+    score_list[i] = score_list[i] / max_score * 100
+
+print(sum(score_list)/test)
